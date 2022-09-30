88986bd469b925a199d3e8c8a7a709b8623f11f0
Jose Paternina <jose.paternina.a@gmail.com>
2022-09-29 19:13:17 -0500

    Added user-products submodule and pom configs

---
+++ user-products/src/test/java/com/secutitypoc/userproducts/UserProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secutitypoc.userproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
